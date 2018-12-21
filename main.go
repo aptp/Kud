00bf77eb5a36a8093ac46fd1087e328701805249
natumn <natsume.pcuse75@gmail.com>
2018-12-21 22:23:15 +0900

    fix main

--- main.go
+++ main.go
@@ -2,68 +2,31 @@ package main
 
 import (
 	"log"
-	"math/rand"
 	"os"
-	"strings"
-	"time"
 
-	"github.com/joho/godotenv"
-	"github.com/kelseyhightower/envconfig"
-	"github.com/nlopes/slack"
+	"github.com/aptp/Kud/adapter/controller"
+	"github.com/aptp/Kud/config"
 )
 
-// Env has some token and key.
-type Env struct {
-	SlackToken string `required:"true"`
-}
-
-var phrase = []string{
-	"WELCOME!!!!!",
-	"THANK YOU!!!!",
-}
-
 func main() {
-	var e Env
-	var err error
 
-	// set config
-	err = godotenv.Load()
+	cfg, err := config.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatalf("Failed on config.Load(): %s\n", err.Error())
 	}
 
-	err = envconfig.Process("klyuchan", &e)
-	if err != nil {
-		log.Fatal(err)
-	}
+	slackBot := controller.NewSlackBot(cfg)
 
-	// application run
-	api := slack.New(e.SlackToken)
-	os.Exit(run(api))
+	os.Exit(
+		run(slackBot),
+	)
 }
 
-func run(api *slack.Client) int {
-	rtm := api.NewRTM()
-	go rtm.ManageConnection()
-
-	for {
-		select {
-		case msg := <-rtm.IncomingEvents:
-			switch ev := msg.Data.(type) {
-			case *slack.HelloEvent:
-				log.Print("Hello Event")
-
-			case *slack.MessageEvent:
-				log.Printf("Message: %v\n", ev)
-				if strings.Contains(ev.Text, "遅れ") {
-					rand.Seed(time.Now().UnixNano())
-					rtm.SendMessage(rtm.NewOutgoingMessage(phrase[rand.Intn(2)], ev.Channel))
-				}
-			case *slack.InvalidAuthEvent:
-				log.Print("Invalid credentials")
-				return 1
-
-			}
-		}
+func run(slackBot controller.SlackBot) int {
+	if err := slackBot.Listen(); err != nil {
+		log.Printf("Error :%s", err.Error())
+		return 1
 	}
+
+	return 0
 }
