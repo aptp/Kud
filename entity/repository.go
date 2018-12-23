950a1ad117c507595182446c3d7f04f4f9b82748
natumn <natsume.pcuse75@gmail.com>
2018-12-23 16:59:15 +0900

    fix define

--- entity/repository.go
+++ entity/repository.go
@@ -8,3 +8,7 @@ import (
 type GitHubRepository interface {
 	GetContributions(ctx context.Context, userName string, from time.Time, to time.Time) ([]int, error)
 }
+
+type SlackRepository interface {
+	GetWorkingCronChannel(ctx context.Context) (string, error)
+}
